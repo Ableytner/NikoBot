8650b83ea6aa6926a7e3e64285e1c9c448832689
Ableytner <ableit190030@sr.htlweiz.at>
2023-11-02 22:14:58 +0100

    update sp2, still broken

--- src/modules/tc4/shortest_path2.py
+++ src/modules/tc4/shortest_path2.py
@@ -42,7 +42,7 @@ class ShortestPath2():
                 pass
             for nb in node.aspect.neighbors:
                 if not nb.name in node.paths.keys():
-                    node.paths[nb.name] = Path(nb.name, nb.cost)
+                    node.paths[nb.name] = Path(nb.name, 0)
         del node, nb
         
         remaining_names = list(self.all_nodes.keys())
@@ -60,7 +60,7 @@ class ShortestPath2():
                 while c2 < len(remaining_names): # for target_node_name in curr_node.paths.keys():
                     node2: Node = self.all_nodes[remaining_names[c2]]
 
-                    if node1.aspect.name == "Nebrisum" and node2.aspect.name == "Nebrisum":
+                    if node1.aspect.name == "Aqua" or node2.aspect.name == "Aqua":
                         pass
                     # print(f"c1: {c1}, c2: {c2}", end="\r")
 
@@ -69,25 +69,21 @@ class ShortestPath2():
                         c2 += 1
                         continue
 
-                    # skiip if no path between the two nodes is knwon
-                    if not node1.aspect.name in node2.paths.keys():
+                    # skip if no path between the two nodes is knwon
+                    if not node1.aspect.name in node2.paths.keys() or not node2.aspect.name in node1.paths.keys():
                         c2 += 1
                         continue
 
                     self._exchange_paths(node1, node2)
 
-                    if len(node1.paths) >= len(self.all_nodes) - 1:
-                        node_name = remaining_names.pop(c1)
-                        self.graph[node_name] = self.all_nodes[node_name]
-                        c1 -= 1
-                        break
-
-                    if len(node2.paths) >= len(self.all_nodes) - 1:
-                        node_name = remaining_names.pop(c2)
-                        self.graph[node_name] = self.all_nodes[node_name]
-                        continue
-
                     c2 += 1
+
+                # if the node has paths to all other nodes, remove it
+                if len(node1.paths) >= len(self.all_nodes) - 1:
+                    node_name = remaining_names.pop(c1)
+                    self.graph[node_name] = self.all_nodes[node_name]
+                    c1 -= 1
+
                 c1 += 1
 
     def _exchange_paths(self, curr_node: Node, other_node: Node):
@@ -96,10 +92,26 @@ class ShortestPath2():
                 continue
 
             if not node_name in curr_node.paths.keys():
-                curr_node.paths[node_name] = Path(other_node.aspect.name, other_path.cost + other_node.aspect.cost)
+                sp = self.recursive(curr_node, other_node)
+                i = 1
+                while i < len(sp) - 1:
+                    node = self.all_nodes[sp[i].name]
+                    if node_name not in node.paths.keys():
+                        node.paths[node_name] = Path(sp[i + 1].name, self._calc_cost(other_node, self.all_nodes[node_name]) + other_node.aspect.cost + self._calc_cost(node, other_node))
+                    i += 1
+
+                curr_node.paths[node_name] = Path(sp[1].name, self._calc_cost(other_node, self.all_nodes[node_name]) + other_node.aspect.cost + self._calc_cost(curr_node, other_node))
             else:
-                if curr_node.paths[node_name].cost > other_path.cost + other_node.aspect.cost:
-                    curr_node.paths[node_name] = Path(other_node.aspect.name, other_path.cost + other_node.aspect.cost)
+                if self._calc_cost(curr_node, self.all_nodes[node_name]) > (self._calc_cost(other_node, self.all_nodes[node_name]) + other_node.aspect.cost + self._calc_cost(curr_node, other_node)):
+                    sp = self.recursive(curr_node, other_node)
+                    i = 1
+                    while i < len(sp) - 1:
+                        node = self.all_nodes[sp[i].name]
+                        if node_name not in node.paths.keys():
+                            node.paths[node_name] = Path(sp[i + 1].name, self._calc_cost(other_node, self.all_nodes[node_name]) + other_node.aspect.cost + self._calc_cost(node, other_node))
+                        i += 1
+
+                    curr_node.paths[node_name] = Path(sp[1].name, self._calc_cost(other_node, self.all_nodes[node_name]) + other_node.aspect.cost + self._calc_cost(curr_node, other_node))
                 else:
                     pass
 
@@ -107,31 +119,63 @@ class ShortestPath2():
             if node_name == other_node.aspect.name:
                 continue
 
-            if not node_name in other_node.paths.keys():
-                other_node.paths[node_name] = Path(curr_node.aspect.name, curr_path.cost + curr_node.aspect.cost)
-            else:
-                if other_node.paths[node_name].cost > curr_path.cost + curr_node.aspect.cost:
-                    other_node.paths[node_name] = Path(curr_node.aspect.name, curr_path.cost + curr_node.aspect.cost)
+            try:
+                if not node_name in other_node.paths.keys():
+                    sp = self.recursive(other_node, curr_node)
+                    i = 1
+                    while i < len(sp) - 1:
+                        node = self.all_nodes[sp[i].name]
+                        if node_name not in node.paths.keys():
+                            node.paths[node_name] = Path(sp[i + 1].name, self._calc_cost(curr_node, self.all_nodes[node_name]) + curr_node.aspect.cost + self._calc_cost(node, curr_node))
+                        i += 1
+
+                    other_node.paths[node_name] = Path(sp[1].name, self._calc_cost(curr_node, self.all_nodes[node_name]) + curr_node.aspect.cost + self._calc_cost(other_node, curr_node))
                 else:
-                    pass
+                    if self._calc_cost(other_node, self.all_nodes[node_name]) > (self._calc_cost(curr_node, self.all_nodes[node_name]) + curr_node.aspect.cost + self._calc_cost(other_node, curr_node)):
+                        sp = self.recursive(other_node, curr_node)
+                        i = 1
+                        while i < len(sp) - 1:
+                            node = self.all_nodes[sp[i].name]
+                            if node_name not in node.paths.keys():
+                                node.paths[node_name] = Path(sp[i + 1].name, self._calc_cost(curr_node, self.all_nodes[node_name]) + curr_node.aspect.cost + self._calc_cost(node, curr_node))
+                            i += 1
+
+                        other_node.paths[node_name] = Path(sp[1].name, self._calc_cost(curr_node, self.all_nodes[node_name]) + curr_node.aspect.cost + self._calc_cost(other_node, curr_node))
+                    else:
+                        pass
+            except:
+                pass
+
+    def _calc_cost(self, start_node: Node, goal_node: Node):
+        cost = 0
+        sp = self.recursive(start_node, goal_node)
+
+        # ignore the current nodes cost
+        for aspect in sp[1:-1:]:
+            cost += aspect.cost
+        
+        return cost
 
     def recursive(self, curr: Node, goal: Node, path = None) -> list[Aspect] | None: # pylint: disable=line-too-long
         """
-        calculate the shortest path, returning a list representing the shortest path
-        every element is the aspect node and the total cost till this point
+        calculate the shortest path, returning a list of aspects representing the shortest path
         """
-
-        ShortestPath2.counter += 1
 
         if path is None:
             path = []
+        c = 0
+        while curr.aspect.name != goal.aspect.name:
+            ShortestPath2.counter += 1
+            c += 1
+            if c > 100000:
+                raise Exception()
+
+            path.append(curr.aspect)
+
+            curr = self.all_nodes[curr.paths[goal.aspect.name].next_node]
 
         path.append(curr.aspect)
-
-        if curr.aspect.name == goal.aspect.name:
-            return path
-
-        return self.recursive(self.all_nodes[curr.paths[goal.aspect.name].next_node], goal, path)
+        return path
 
     def print_to_file(self):
         with open("graph.txt", "w+") as f:
