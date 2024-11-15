cf4d35bdad5121a81c838c124892dce43d622525
Ableytner <56540036+Ableytner@users.noreply.github.com>
2024-11-15 19:49:20 +0100

    add command error handling (#3)
    * add command error handling
    
    * pylint

--- src/nikobot/util/discord.py
+++ src/nikobot/util/discord.py
@@ -12,6 +12,14 @@ from discord.ext import commands
 from . import error
 from .storage import VolatileStorage
 
+def get_command_name(ctx: commands.context.Context | discord.interactions.Interaction) -> str:
+    """Return the full name of the contexts' command"""
+
+    if not is_slash_command(ctx):
+        return ctx.command.qualified_name
+
+    return ctx.command.qualified_name
+
 def get_bot() -> commands.Bot:
     """Return the ``DiscordBot`` instance"""
 
@@ -24,6 +32,27 @@ def get_bot() -> commands.Bot:
         raise TypeError()
 
     return bot
+
+def get_owner_id() -> int:
+    """Return the discord bot owners' user_id"""
+
+    return 650587171375284226
+
+async def get_reply(ctx: commands.context.Context | discord.interactions.Interaction) \
+          -> discord.Message | discord.interactions.InteractionMessage | None:
+    """Return the reply message for the given context, or None if the bot didn't reply yet"""
+
+    if not is_slash_command(ctx):
+        ctx: commands.context.Context = ctx
+        async for message in ctx.channel.history(limit=100):
+            if message.reference is not None and message.reference.message_id == ctx.message.id:
+                return message
+        return None
+
+    if not ctx.response.is_done():
+        return None
+
+    return await ctx.original_response()
 
 def get_user_id(ctx: commands.context.Context | discord.interactions.Interaction) -> int:
     """Get discords user id from the message's sender"""
@@ -193,7 +222,7 @@ def grouped_hybrid_command(name: str, description: str, command_group: app_comma
         return wrapper
     return decorator
 
-def is_slash_command(ctx: commands.context.Context | discord.interactions.Interaction):
+def is_slash_command(ctx: commands.context.Context | discord.interactions.Interaction) -> bool:
     """Checks whether the ``ctx`` was received from a 'normal' text command or a slash command"""
 
     # for slash commands
