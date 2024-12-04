68d7b767f34bbd28b80329d1425f18756ae9c837
Ableytner <56540036+Ableytner@users.noreply.github.com>
2024-12-04 04:49:01 +0100

    Storage rework (#8)
    * rework storages, add StorageView
    
    * implement changes + fix typo
    
    * pylint
    
    * dynamic StorageView creation

--- src/nikobot/util/storage.py
+++ src/nikobot/util/storage.py
@@ -15,7 +15,7 @@ class _BaseStorage():
 
     _store: dict[str, Any] = None
 
-    def contains(self, key: str, item: Any) -> bool:
+    def contains_item(self, key: str, item: Any) -> bool:
         """
         Checks whether a key within the storage contains an item
         If 'key' contains a '.', also checks if all sub-dicts exist
@@ -24,21 +24,21 @@ class _BaseStorage():
         if not isinstance(key, str):
             raise TypeError()
 
-        if not self.exists(key):
+        if not self.contains(key):
             return False
-        return item in self[key]
+        return item == self[key]
 
-    def exists(self, key: str) -> bool:
+    def contains(self, key: str) -> bool:
         """
         Checks whether a key exists within the storage
         If 'key' contains a '.', also checks if all sub-dicts exist
         """
+        # allows checking multi-layer dicts with the following format:
+        # util.PersistentStorage["some_module.some_subdict.another_subdict.key"]
 
         if not isinstance(key, str):
             raise TypeError()
 
-        # allows checking multi-layer dicts with the following format:
-        # util.PersistentStorage["some_module.some_subdict.another_subdict.key"]
         if "." not in key:
             return key in self._store
 
@@ -54,17 +54,23 @@ class _BaseStorage():
         return parts[-1] in curr_dict
 
     def __getitem__(self, key: str) -> Any:
+        # allows getting multi-layer dicts with the following format:
+        # util.PersistentStorage["some_module.some_subdict.another_subdict.key"]
+
         if not isinstance(key, str):
             raise TypeError()
 
-        # allows getting multi-layer dicts with the following format:
-        # util.PersistentStorage["some_module.some_subdict.another_subdict.key"]
         if "." not in key:
+            if key not in self._store:
+                raise error.KeyNotFound("Key not found in storage: " + key)
             return self._store[key]
 
         parts = key.split(".")
         curr_dict = self._store
         for c, part in enumerate(parts):
+            if part not in curr_dict:
+                raise error.KeyNotFound("Key not found in storage: " +
+                                        ".".join([item if item != part else f"'{item}'" for item in parts]))
             # if it isn't the last part
             if c < len(parts) - 1:
                 curr_dict = curr_dict[part]
@@ -72,55 +78,63 @@ class _BaseStorage():
         return curr_dict[parts[-1]]
 
     def __setitem__(self, key: str, item: Any) -> None:
-        if not isinstance(key, str):
-            raise TypeError()
-
         # allows adding multi-layer dicts with the following format:
         # util.PersistentStorage["some_module.some_subdict.another_subdict.key"] = "value"
-        # items can be accessed using:
-        # util.PersistentStorage["some_module"]["some_subdict"]["another_subdict"]["key"]
-        if "." in key:
-            parts = key.split(".")
-            curr_dict = self._store
-            for c, part in enumerate(parts):
-                # if it isn't the last part
-                if c < len(parts) - 1:
-                    # add a missing dictionary
-                    if part not in curr_dict:
-                        curr_dict[part] = {}
-                    curr_dict = curr_dict[part]
-                else:
-                    # add the actual value
-                    curr_dict[part] = item
-        else:
-            self._store[key] = item
 
-    def __delitem__(self, key: str) -> None:
         if not isinstance(key, str):
             raise TypeError()
 
+        if "." not in key:
+            self._store[key] = item
+            return
+
+        parts = key.split(".")
+        curr_dict = self._store
+        for c, part in enumerate(parts):
+            # if it isn't the last part
+            if c < len(parts) - 1:
+                # add a missing dictionary
+                if part not in curr_dict:
+                    curr_dict[part] = {}
+                curr_dict = curr_dict[part]
+            else:
+                # add the actual value
+                curr_dict[part] = item
+
+    def __delitem__(self, key: str) -> None:
         # items can be removed using:
         # del util.PersistentStorage["some_module"]["some_subdict"]["another_subdict"]["key"]
         # or
         # del util.PersistentStorage["some_module.some_subdict.another_subdict.key"]
-        if "." in key:
-            parts = key.split(".")
-            curr_dict = self._store
-            for c, part in enumerate(parts):
-                # if it isn't the last part
-                if c < len(parts) - 1:
-                    # if a directory is missing, the key definitly doesn't exist
-                    if part not in curr_dict:
-                        return
-                    curr_dict = curr_dict[part]
-                else:
-                    # delete the actual key
-                    del curr_dict[part]
-        else:
+
+        if not isinstance(key, str):
+            raise TypeError()
+
+        if "." not in key:
+            if key not in self._store:
+                raise error.KeyNotFound("Key not found in storage: " + key)
             del self._store[key]
+            return
+
+        parts = key.split(".")
+        curr_dict = self._store
+        for c, part in enumerate(parts):
+            if part not in curr_dict:
+                raise error.KeyNotFound("Key not found in storage: " +
+                                        ".".join([item if item != part else f"'{item}'" for item in parts]))
+
+            # if it isn't the last part
+            if c < len(parts) - 1:
+                # if a directory is missing, the key definitly doesn't exist
+                if part not in curr_dict:
+                    return
+                curr_dict = curr_dict[part]
+            else:
+                # delete the actual key
+                del curr_dict[part]
 
     def __contains__(self, key: str) -> bool:
-        return key in self._store
+        return self.contains(key)
 
     def __str__(self) -> str:
         return str(self._store)
@@ -152,10 +166,10 @@ class _PersistentStorage(_BaseStorage):
         return super().__setitem__(key, item)
 
     def _load_from_disk(self) -> None:
-        if "storage_file" not in VolatileStorage:
+        if "storage_file" not in StorageView:
             raise error.KeyNotFound()
 
-        path = VolatileStorage["storage_file"]
+        path = StorageView["storage_file"]
         if not os.path.isfile(path):
             print("Storage file doesn't yet exist")
             return
@@ -164,10 +178,10 @@ class _PersistentStorage(_BaseStorage):
             self._store = json.load(f)
 
     def _save_to_disk(self) -> None:
-        if "storage_file" not in VolatileStorage:
+        if "storage_file" not in StorageView:
             raise error.KeyNotFound()
 
-        path = VolatileStorage["storage_file"]
+        path = StorageView["storage_file"]
         if len(self._store) == 0 and os.path.isfile(path):
             print("Not overwriting existing storage file with empty storage")
             return
@@ -175,8 +189,51 @@ class _PersistentStorage(_BaseStorage):
         with open(path, "w", encoding="utf8") as f:
             json.dump(self._store, f)
 
+class _StorageView():
+    """A read-only view on both the PersistentStorage and VolatileStorage"""
+
+    def __init__(self, storages: list[_BaseStorage]):
+        for storage in storages:
+            if not isinstance(storage, _BaseStorage):
+                raise TypeError()
+            self._storages.append(storage)
+
+    _storages = []
+
+    def contains_item(self, key: str, item: Any) -> bool:
+        """
+        Checks whether a key within the storage contains an item
+        If 'key' contains a '.', also checks if all sub-dicts exist
+        """
+
+        for storage in self._storages:
+            if storage.contains_item(key, item):
+                return True
+        return False
+
+    def contains(self, key: str) -> bool:
+        """
+        Checks whether a key exists within the storage
+        If 'key' contains a '.', also checks if all sub-dicts exist
+        """
+
+        for storage in self._storages:
+            if storage.contains(key):
+                return True
+        return False
+
+    def __getitem__(self, key: str) -> Any:
+        for storage in self._storages:
+            if key in storage:
+                return storage[key]
+        raise error.KeyNotFound()
+
+    def __contains__(self, key: str) -> bool:
+        return self.contains(key)
+
 VolatileStorage = _VolatileStorage()
 PersistentStorage = _PersistentStorage()
+StorageView = _StorageView([VolatileStorage, PersistentStorage])
 
 # save persistent storage before program exits
 # pylint: disable-next=protected-access
