778cea03b276a07b753bfea8e93a1bde0eda2fe5
Ableytner <emanuel.ableitner@gmx.at>
2025-03-06 20:20:20 +0100

    bump version

--- src/nikobot/util/log_helper.py
+++ src/nikobot/util/log_helper.py
@@ -6,17 +6,23 @@ import sys
 
 import discord as discordpy
 
+LOG_LEVEL = logging.INFO
+
 def setup():
     """Setup the log handlers"""
 
+    if "DEBUG" in os.environ:
+        global LOG_LEVEL
+        LOG_LEVEL = logging.DEBUG
+
     stream_handler = logging.StreamHandler(sys.stderr)
     discordpy.utils.setup_logging(handler=stream_handler,
-                                  level=logging.INFO if "DEBUG" not in os.environ else logging.DEBUG,
+                                  level=LOG_LEVEL,
                                   root=True)
 
     file_handler = logging.FileHandler(filename="latest.log", encoding="utf-8", mode="w")
     discordpy.utils.setup_logging(handler=file_handler,
-                                  level=logging.INFO if "DEBUG" not in os.environ else logging.DEBUG,
+                                  level=LOG_LEVEL,
                                   root=True)
 
 def setup_for_tests():
